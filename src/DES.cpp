29be724ea7b8507699cf8ffe03ba5a5afbeb0d9e
Kyle Akerstrom <kakerstrom@gmail.com>
2019-09-27 10:50:32 -0600

    Added more implementation to Encrypt function, started on InitialPermutation

--- src/DES.cpp
+++ src/DES.cpp
@@ -1,5 +1,7 @@
 #include "DES.h"
 #include "Exceptions.h"
+#include <vector>
+#include <bitset>
 
 DES::DES()
 {
@@ -11,12 +13,34 @@ BitField DES::Encrypt(BitField data, BitField key)
   if(key.GetLength() != 8)
     throw DESException("Key must be 64 bits.");
 
+  // Split the input data into 64-bit chunks
+  std::vector<BitField> chunks;
 
-  for (int i = 0; i < 16; i++)
+  for(int i = 0; i < data.GetLength() % 8; i++)
   {
-
-    Round(data, key);
+    // Break the data into 64 bit chunks
+    BitField b(8);
+    for(int j = 0; j < 8; j++)
+    {
+      if(data.GetLength() > i+j)
+        b.SetByte(j, data.GetBytes()[i+j]);
+      else
+        b.SetByte(j, 0); // Pad the final chunk to be a full 64 bits
+    }
+    chunks.push_back(b);
   }
+
+  // Initial Permutation
+  chunks[0] = InitialPermutation(chunks[0]);
+
+  // Generate keys
+
+  std::string cipher;
+  for(BitField b : chunks)
+    for (int i = 0; i < 16; i++)
+    {
+      Round(data, key);
+    }
 }
 
 BitField DES::Decrypt(BitField data, BitField key)
@@ -31,7 +55,11 @@ BitField DES::Round(BitField data, BitField key)
 
 BitField DES::InitialPermutation(BitField data)
 {
+  std::bitset<64> input;
+  for(int i = 0; i < 64; i++)
+  {
 
+  }
 }
 
 BitField DES::InverseInitialPermutation(BitField data)
